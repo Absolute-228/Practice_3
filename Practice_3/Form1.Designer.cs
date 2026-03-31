ba97db7b6c52960f9b4b40dc6cb18176da998b57
dsfsdf <rusolnicikita>
2026-03-31 15:27:00 +0300

    Добавил таблицу и поля для добавления данных

--- Practice_3/Form1.Designer.cs
+++ Practice_3/Form1.Designer.cs
@@ -29,11 +29,25 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textGroupName = new System.Windows.Forms.TextBox();
+            this.textSubjectName = new System.Windows.Forms.TextBox();
+            this.textTeacherName = new System.Windows.Forms.TextBox();
+            this.textTotalHours = new System.Windows.Forms.TextBox();
+            this.textExamType = new System.Windows.Forms.TextBox();
+            this.textLabHours = new System.Windows.Forms.TextBox();
+            this.textPracticeHours = new System.Windows.Forms.TextBox();
+            this.textLectureHours = new System.Windows.Forms.TextBox();
+            this.textYear = new System.Windows.Forms.TextBox();
+            this.textDepartment = new System.Windows.Forms.TextBox();
+            this.textCredits = new System.Windows.Forms.TextBox();
+            this.textSemester = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(54, 175);
+            this.button1.Location = new System.Drawing.Point(1184, 664);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 28);
             this.button1.TabIndex = 0;
@@ -41,21 +55,154 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textGroupName
+            // 
+            this.textGroupName.Location = new System.Drawing.Point(88, 76);
+            this.textGroupName.Name = "textGroupName";
+            this.textGroupName.Size = new System.Drawing.Size(100, 26);
+            this.textGroupName.TabIndex = 1;
+            // 
+            // textSubjectName
+            // 
+            this.textSubjectName.Location = new System.Drawing.Point(88, 157);
+            this.textSubjectName.Name = "textSubjectName";
+            this.textSubjectName.Size = new System.Drawing.Size(100, 26);
+            this.textSubjectName.TabIndex = 2;
+            // 
+            // textTeacherName
+            // 
+            this.textTeacherName.Location = new System.Drawing.Point(88, 265);
+            this.textTeacherName.Name = "textTeacherName";
+            this.textTeacherName.Size = new System.Drawing.Size(100, 26);
+            this.textTeacherName.TabIndex = 3;
+            // 
+            // textTotalHours
+            // 
+            this.textTotalHours.Location = new System.Drawing.Point(88, 365);
+            this.textTotalHours.Name = "textTotalHours";
+            this.textTotalHours.Size = new System.Drawing.Size(100, 26);
+            this.textTotalHours.TabIndex = 4;
+            // 
+            // textExamType
+            // 
+            this.textExamType.Location = new System.Drawing.Point(289, 365);
+            this.textExamType.Name = "textExamType";
+            this.textExamType.Size = new System.Drawing.Size(100, 26);
+            this.textExamType.TabIndex = 8;
+            // 
+            // textLabHours
+            // 
+            this.textLabHours.Location = new System.Drawing.Point(289, 265);
+            this.textLabHours.Name = "textLabHours";
+            this.textLabHours.Size = new System.Drawing.Size(100, 26);
+            this.textLabHours.TabIndex = 7;
+            // 
+            // textPracticeHours
+            // 
+            this.textPracticeHours.Location = new System.Drawing.Point(289, 157);
+            this.textPracticeHours.Name = "textPracticeHours";
+            this.textPracticeHours.Size = new System.Drawing.Size(100, 26);
+            this.textPracticeHours.TabIndex = 6;
+            // 
+            // textLectureHours
+            // 
+            this.textLectureHours.Location = new System.Drawing.Point(289, 76);
+            this.textLectureHours.Name = "textLectureHours";
+            this.textLectureHours.Size = new System.Drawing.Size(100, 26);
+            this.textLectureHours.TabIndex = 5;
+            // 
+            // textYear
+            // 
+            this.textYear.Location = new System.Drawing.Point(529, 365);
+            this.textYear.Name = "textYear";
+            this.textYear.Size = new System.Drawing.Size(100, 26);
+            this.textYear.TabIndex = 12;
+            // 
+            // textDepartment
+            // 
+            this.textDepartment.Location = new System.Drawing.Point(529, 265);
+            this.textDepartment.Name = "textDepartment";
+            this.textDepartment.Size = new System.Drawing.Size(100, 26);
+            this.textDepartment.TabIndex = 11;
+            // 
+            // textCredits
+            // 
+            this.textCredits.Location = new System.Drawing.Point(529, 157);
+            this.textCredits.Name = "textCredits";
+            this.textCredits.Size = new System.Drawing.Size(100, 26);
+            this.textCredits.TabIndex = 10;
+            // 
+            // textSemester
+            // 
+            this.textSemester.Location = new System.Drawing.Point(529, 76);
+            this.textSemester.Name = "textSemester";
+            this.textSemester.Size = new System.Drawing.Size(100, 26);
+            this.textSemester.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(65, 690);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 39);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(480, 690);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(115, 39);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Создать дб";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1360, 818);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textYear);
+            this.Controls.Add(this.textDepartment);
+            this.Controls.Add(this.textCredits);
+            this.Controls.Add(this.textSemester);
+            this.Controls.Add(this.textExamType);
+            this.Controls.Add(this.textLabHours);
+            this.Controls.Add(this.textPracticeHours);
+            this.Controls.Add(this.textLectureHours);
+            this.Controls.Add(this.textTotalHours);
+            this.Controls.Add(this.textTeacherName);
+            this.Controls.Add(this.textSubjectName);
+            this.Controls.Add(this.textGroupName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textGroupName;
+        private System.Windows.Forms.TextBox textSubjectName;
+        private System.Windows.Forms.TextBox textTeacherName;
+        private System.Windows.Forms.TextBox textTotalHours;
+        private System.Windows.Forms.TextBox textExamType;
+        private System.Windows.Forms.TextBox textLabHours;
+        private System.Windows.Forms.TextBox textPracticeHours;
+        private System.Windows.Forms.TextBox textLectureHours;
+        private System.Windows.Forms.TextBox textYear;
+        private System.Windows.Forms.TextBox textDepartment;
+        private System.Windows.Forms.TextBox textCredits;
+        private System.Windows.Forms.TextBox textSemester;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
--- Practice_3/Form1.cs
+++ Practice_3/Form1.cs
@@ -29,5 +29,80 @@ namespace Practice_3
             }
 
         }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=practice_3.db";
+
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+
+                string sql = @" 
+                  CREATE TABLE IF NOT EXISTS Practice_3(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Year INTEGER,
+                    GroupName TEXT,
+                 SubjectName TEXT,
+                 TeacherName TEXT,
+                 TotalHours INTEGER,
+                 LectureHours INTEGER,
+                 PracticeHours INTEGER,
+                 LabHours INTEGER,
+                 ExamType TEXT,
+                 Semester INTEGER,
+                 Credits INTEGER,
+                 Department TEXT
+                )";
+
+                using (var cmd = new  SQLiteCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Таблица создана");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=practice_3.db;";
+
+            using (var db = new SQLiteConnection(connectionString))
+            {
+                db.Open();
+
+                string sql = @"
+        INSERT INTO StudyPlans 
+        (Year, GroupName, SubjectName, TeacherName, TotalHours,
+         LectureHours, PracticeHours, LabHours, ExamType,
+         Semester, Credits, Department)
+        VALUES
+        (@Year, @GroupName, @SubjectName, @TeacherName, @TotalHours,
+         @LectureHours, @PracticeHours, @LabHours, @ExamType,
+         @Semester, @Credits, @Department);";
+
+                using (var cmd = new SQLiteCommand(sql, db))
+                {
+                    cmd.Parameters.AddWithValue("@Year", int.Parse(textYear.Text));
+                    cmd.Parameters.AddWithValue("@GroupName", textGroupName.Text);
+                    cmd.Parameters.AddWithValue("@SubjectName", textSubjectName.Text);
+                    cmd.Parameters.AddWithValue("@TeacherName", textTeacherName.Text);
+                    cmd.Parameters.AddWithValue("@TotalHours", int.Parse(textTotalHours.Text));
+
+                    cmd.Parameters.AddWithValue("@LectureHours", int.Parse(textLectureHours.Text));
+                    cmd.Parameters.AddWithValue("@PracticeHours", int.Parse(textPracticeHours.Text));
+                    cmd.Parameters.AddWithValue("@LabHours", int.Parse(textLabHours.Text));
+
+                    cmd.Parameters.AddWithValue("@ExamType", textExamType.Text);
+                    cmd.Parameters.AddWithValue("@Semester", int.Parse(textSemester.Text));
+                    cmd.Parameters.AddWithValue("@Credits", int.Parse(textCredits.Text));
+                    cmd.Parameters.AddWithValue("@Department", textDepartment.Text);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Добавлено!");
+            }
+        }
     }
 }
